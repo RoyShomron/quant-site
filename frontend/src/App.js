fead40e13a6456a2eb5f71cc4045e9bae41c2680
Roy Shomron <royshomron2@gmail.com>
2026-06-02 15:14:42 -0400

    Added analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Backtest from "./Backtest";
 import Learn from "./Learn";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/backtest" element={<Backtest />} />
         <Route path="/learn" element={<Learn />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
